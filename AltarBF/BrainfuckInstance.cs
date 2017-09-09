3e2f923bc21d8e6de201ad7232b48ef91752dab1
Marc Dumoulin <mdumoulin.home@gmail.com>
2017-09-09 15:59:38 -0400

    Add project files.

---
+++ AltarBF/BrainfuckInstance.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AltarBF {
+	public class BrainfuckInstance {
+		public const UInt16 MEMSIZE = 30000;
+
+		private readonly string Code;
+		private readonly TextReader Reader;
+		private readonly TextWriter Writer;
+		private readonly Stack<int> Loops;
+		private readonly byte[] Memory;
+		private UInt16 Ptr;
+		private UInt16 InnerLoopsCounter;
+
+		public BrainfuckInstance(string code, TextReader reader, TextWriter writer) {
+			Code = code;
+			Reader = reader;
+			Writer = writer;
+			Memory = new byte[MEMSIZE];
+			Loops = new Stack<int>();
+			Ptr = 0;
+			Run();
+		}
+
+		private void Run() {
+			for (int i = 0; i < Code.Length; i++) {
+				switch (Code[i]) {
+					case '+':
+						Memory[Ptr]++;
+						break;
+					case '-':
+						Memory[Ptr]--;
+						break;
+					case '>':
+						if (++Ptr == MEMSIZE)
+							Ptr = 0;
+						break;
+					case '<':
+						if (Ptr == 0)
+							Ptr = MEMSIZE - 1;
+						else --Ptr;
+						break;
+					case '.':
+						Writer.Write((char)Memory[Ptr]);
+						Writer.Flush();
+						break;
+					case ',':
+						Memory[Ptr] = (byte)(Reader.Peek() != -1 ? Reader.Read() : 0);
+						break;
+					case '[':
+						if (Memory[Ptr] == 0) {
+							while (++i < Code.Length) {
+								if (Code[i] == '[')
+									InnerLoopsCounter++;
+								else if (Code[i] == ']') {
+									if (InnerLoopsCounter == 0)
+										break;
+									InnerLoopsCounter--;
+								}
+							}
+							if (InnerLoopsCounter != 0)
+								throw new Exception("No matching ] found!");
+						} else Loops.Push(i);
+						break;
+					case ']':
+						if (Memory[Ptr] == 0)
+							Loops.Pop();
+						else i = Loops.Peek();
+						break;
+				}
+			}
+		}
+	}
+}
---
+++ AltarBF/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace AltarBF {
+	class Program {
+		public const string HelloWorldWithNewLine = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+		public const string ReadAllInput = ",[.,]";
+		public const string InnerLoopTest = "[.+[-]+]+.";
+
+		static void Main(string[] args) {
+			using (var reader = GenerateStreamFromString("Test")) {
+				new BrainfuckInstance(InnerLoopTest, reader, Console.Out);
+				new BrainfuckInstance(HelloWorldWithNewLine, reader, Console.Out);
+				new BrainfuckInstance(ReadAllInput, reader, Console.Out);
+			}
+			Console.WriteLine();
+			Console.WriteLine("-------------------");
+			Console.WriteLine("Done!");
+			Console.ReadKey();
+		}
+
+		public static StreamReader GenerateStreamFromString(string s) {
+			var stream = new MemoryStream();
+			using (var writer = new StreamWriter(stream, Encoding.ASCII, s.Length, true)) {
+				writer.Write(s);
+				writer.Flush();
+				stream.Position = 0;
+			}
+			return new StreamReader(stream);
+		}
+	}
+}
